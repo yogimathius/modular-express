9181b534cac2092880445075cc016fa4fc77aeb8
Mathius <info@yogimathius.dev>
2023-11-01 21:30:18 -0700

    chore: reset to Sept 18 cohort starting point

--- server.js
+++ server.js
@@ -1,145 +1,97 @@
 const express = require("express");
-const morgan = require("morgan");
-// const cookieParser = require("cookie-parser");
-const cookieSession = require("cookie-session");
-const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
 
 const app = express();
-const PORT = 3000;
-const SALT_ROUNDS = 10;
-const salt = bcrypt.genSaltSync(SALT_ROUNDS);
-const cookieSessionConfig = {
-  name: "myLoginSession",
-  keys: ["my-secret-key"],
-  maxAge: 24 * 60 * 60 * 1000, // 24 hours
-};
 
-app.use(cookieSession(cookieSessionConfig));
-app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+// Setting up view envine
 app.set("view engine", "ejs");
 
-app.listen(PORT, () =>
-  console.log(`My fancy server is listening on port: ${PORT}`)
-);
-
-// will not persist between server restarts
-const users = {
-  123: {
-    id: "123",
-    email: "morty@notRick.com",
-    password: "notCats",
+// Simulation of a database
+const listOfUsers = {
+  "alex@miller.com": {
+    firstName: "Alex",
+    lastName: "Miller",
+    password: "password123",
   },
-  234: {
-    id: "234",
-    email: "bender@robot.com",
-    password: "cigar",
+  "martha@smith.com": {
+    firstName: "Martha",
+    lastName: "Smith",
+    password: "secret1234",
   },
 };
 
-const findUserByEmail = (email) => {
-  for (const userId in users) {
-    if (users[userId].email === email) {
-      return users[userId];
-    }
-  }
-};
-
-app.get("/", (req, res) => {
-  console.log("cookies obj =============>", req.session);
-  // const user = users[req.cookies.userID];
-  const user = users[req.session.userID];
-  res.render("index", { user });
-});
-
-app.get("/register", (req, res) => {
-  res.render("register");
-});
-
-app.post("/register", (req, res) => {
-  const { email, password } = req.body;
-
-  // check if email or password is undefined (the client didn't provide a value)
-  if (!email || !password) {
-    return res.status(400).send("please provide an email and a password");
-  }
-
-  // create a random id
-  const id = Math.random().toString(36).substring(2, 5);
-  // create a new user object
-
-  const hashedPassword = bcrypt.hashSync(password, salt);
-
-  console.log("hashing password: ", hashedPassword);
-
-  const user = {
-    id,
-    email,
-    password: hashedPassword, // Store our hashed password instead of our plain text!
-  };
-
-  // add the new user object to `users`
-  users[id] = user;
-
-  console.log("Updated Users after Register:", users);
-
-  // redirect the client to the login page
-  res.redirect("/login");
+// Endpoints
+app.get("/registration", (req, res) => {
+  return res.render("registration");
 });
 
 app.get("/login", (req, res) => {
-  res.render("login");
+  return res.render("login");
 });
 
-app.post("/login", (req, res) => {
-  console.log("req.body", req.body);
-  const email = req.body.email;
-  const password = req.body.password;
-
-  if (!email || !password) {
-    return res.status(400).send("email and password cannot be blank");
+app.get("/home", (req, res) => {
+  if (!req.cookies.firstName) {
+    console.log("You are not allowed to be here!");
+    return res.redirect("/login");
   }
 
-  const user = findUserByEmail(email);
-  console.log("user obj", user);
-
-  if (!user) {
-    return res.status(400).send("a user with that email does not exist");
-  }
-  console.log("user password from db: ", user.password);
-  console.log("password from login: ", password);
-
-  const isMatch = bcrypt.compareSync(password, user.password);
-
-  if (!isMatch) {
-    return res.status(400).send("password does not match");
-  }
-
-  // res.cookie("userID", user.id);
-  req.session.userID = user.id;
-  res.redirect("/");
+  const templateVariables = {
+    firstName: req.cookies.firstName,
+    lastName: req.cookies.lastName,
+  };
+  return res.render("home", templateVariables);
 });
 
-app.get("/protected", (req, res) => {
-  // const userID = req.cookies.userID;
-  const userID = req.session.userID;
-  if (!userID) {
-    return res.status(401).send("Unauthorized");
+app.post("/create/user", (req, res) => {
+  // If the user already exists, don't create it again. Send an error message
+  if (listOfUsers[req.body.email]) {
+    return res.status(400).send(`
+                    <h1>The email that you are trying to use already exists!</h1>
+                    <a href="/registration"> Go back to registration </a>
+                    `);
   }
 
-  const foundUser = users[userID];
+  listOfUsers[req.body.email] = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    password: req.body.password,
+  };
 
-  if (!foundUser) {
-    return res.status(401).send("Invalid Cookie");
+  res.cookie("firstName", req.body.firstName);
+  res.cookie("lastName", req.body.lastName);
+
+  // Redirect to home page
+  return res.redirect("/home");
+});
+
+app.post("/process/login", (req, res) => {
+  // Validate if email exists in the listOfUsers
+  if (!listOfUsers[req.body.email]) {
+    return res.send("<h1>This user doesn't exist!</h1>");
   }
 
-  res.render("protected", foundUser);
+  // Validate that the passwords match
+  if (listOfUsers[req.body.email].password !== req.body.password) {
+    return res.send("<h1>Wrong credentials</h1>");
+  }
+
+  res.cookie("firstName", listOfUsers[req.body.email].firstName);
+  res.cookie("lastName", listOfUsers[req.body.email].lastName);
+
+  return res.redirect("/home");
 });
 
 app.post("/logout", (req, res) => {
-  // res.clearCookie("userID");
-  req.session = null;
-  res.redirect("/login");
+  res.clearCookie("firstName");
+  res.clearCookie("lastName");
+  return res.redirect("/login");
+});
+
+app.listen(8080, () => {
+  console.log("Server running in port 8080");
 });
